f1f0ee57913e6df591f94f1e891d0b481ad7586c
Zou Yi <intoner.dev@gmail.com>
2018-05-04 01:46:57 -0700

    pcard finsihed

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ var inpTo = document.getElementById("inpTo"),
     addToButton = document.getElementById("addToGallery"),
     saveButton = document.getElementById("saveArr"),
     loadButton = document.getElementById("loadArr"),
-    postArr = [];
+    postArr = [],
+    bgURL,
+    autoMode = 0,
+    arrInd = 0;
 
 
 var dispTo = document.getElementById("to"),
@@ -41,51 +44,80 @@ mymsg.addEventListener("keyup", function(){
 inpBg.addEventListener("keyup", function(event){
     
     if(event.keyCode == 13){
-        console.log(event.keyCode);
+        //console.log(event.keyCode);
                 
-        console.log(this.value);
+       // console.log(this.value);
 
         
          if(this.value == "auto"){
            
-                   console.log("i am in");
-           
+           autoMode = 1;
            
              if(selection == 1){
                     document.getElementById("postcard").style.backgroundImage = 'url("imgs/auto1.jpg")';
+                    bgURL = 'url("imgs/auto1.jpg")';
+               
+               
             selection++;
 
              } else if(selection ==2){
                    document.getElementById("postcard").style.backgroundImage = "url('imgs/auto2.jpg')";
+                    bgURL = 'url("imgs/auto2.jpg")';
 
 
             selection++;
           } else if(selection ==3){
 
                      document.getElementById("postcard").style.backgroundImage = "url('imgs/auto3.jpg')";
-
-
+                      bgURL = 'url("imgs/auto3.jpg")';
             selection = 1;
-          } else if(this.value==""){
+          }
+
+        } else if(this.value==""){
 
               document.getElementById("postcard").style.backgroundImage = "url('imgs/default.png')";
+                     autoMode =0;
 
-        } else {
-                  postDiv.style.backgroundImage = "url("+inpBg.value+")";
+        
+        }else {
+            postDiv.style.backgroundImage = "url("+inpBg.value+")";
+          
+              autoMode =0;
+
 
         }
     
     }
     
-  }
-})
+  })
 
 
 addToButton.addEventListener("click", function(){
     
-      var obj = {
+  //console.log(autoMode);
+  var bgInput;
+    if(autoMode == 1){
+      bgInput = bgURL;
+      
+      
+    } else {
+      
+      if(inpBg.value == ""){
         
-        bgimg :inpBg.value,
+        bgInput = "url('imgs/default.png')";
+      } else {
+        
+        bgInput = inpBg.value;
+
+      }
+    }
+    
+    
+  console.log("bginput"+bgInput);
+  
+    var obj = {
+        
+        bgimg :bgInput,
         to :inpTo.value, 
         message :mymsg.value,
         from :inpFrom.value
@@ -95,48 +127,58 @@ addToButton.addEventListener("click", function(){
   console.log(postArr);
   
   
-      createPostcard(inpTo.value, inpBg.value);
+      createPostcard(inpTo.value, bgInput, inpFrom.value,mymsg.value);
                       
 })
 
-function createPostcard (to, bgImg){
+function createPostcard (to, bgImg, from, msg){
    var miniCard = document.createElement("div");
     var miniName = document.createElement("div");
-    
+  
     miniName.setAttribute("class", "miniName");
     miniName.innerHTML = to;
     miniCard.setAttribute("class", "miniDiv");
-    miniCard.style.backgroundImage = "url("+bgImg+")";
+    
+    miniCard.addEventListener("click", function(){
+       dispTo.innerHTML = to;
+  dispFrom.innerHTML =from;
+  dispMsg.innerHTML = msg;
+      
+          if(bgImg.slice(0, 3)=="url"){
+        
+            postDiv.style.backgroundImage = bgImg;
+
+      } else {
+
+          postDiv.style.backgroundImage =  "url("+bgImg+")";
+
+        
+        
+      }
+    }); 
   
-  
-  
+  //console.log("bgImg is"+bgImg);
+      if(bgImg.slice(0, 3)=="url"){
+        
+            miniCard.style.backgroundImage = bgImg;
+
+      } else {
+
+          miniCard.style.backgroundImage =  "url("+bgImg+")";
+
+        
+        
+      }
+
+
     miniCard.appendChild(miniName);
     previewDiv.appendChild(miniCard);
   
 }
 
 saveButton.addEventListener("click", function(){
-  
-  
-    var mypostcards = JSON.parse(localStorage.getItem("postcards"));
-  
-
-      for(var i=0; i<postArr.length; i++){
-        
-                var obj = {
-        
-        bgimg :postArr[i].bgimg,
-to :postArr[i].to,
-message :postArr[i].message,
-from :ipostArr[i].from,
-    }
-    
-            mypostcards.push(obj);
-
-      }
-  
-
-    localStorage.setItem("postcards",  JSON.stringify(mypostcards));
+      
+      localStorage.setItem('postcards', JSON.stringify(postArr));
 
   
 })
@@ -146,11 +188,11 @@ loadButton.addEventListener("click", function(){
   
     previewDiv.innerHTML = "";
 
-    var mypostcards = JSON.parse(localStorage.getItem("postcards"));
+    postArr = JSON.parse(localStorage.getItem("postcards"));
     
-    for (var i=0; i<mypostcards.length; i++){
-      
-      createPostcard(mypostcards[i].to, mypostcards[i].bgimg);
+    for (var i=0; i<postArr.length; i++){
+      console.log(postArr[i].to);
+      createPostcard(postArr[i].to, postArr[i].bgimg,  postArr[i].from,postArr[i].message);
     }
 
   
